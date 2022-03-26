9b97634b4f6b60f56dd5d5009a9126453d4cea42
Zefanja Jobse <zefanjaj@gmail.com>
2022-03-26 13:52:17 +0100

    undo

--- gather-standalone/Jwt.cs
+++ gather-standalone/Jwt.cs
@@ -22,7 +22,7 @@ namespace gather_standalone.Properties
                     new Claim( "post", dataString)
                 }),
                 Expires = now.AddMinutes(3),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ConfigurationManager.AppSettings["jwtSecret"])), SecurityAlgorithms.HmacSha256),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SUPERSECRETPLACEHOLDER")), SecurityAlgorithms.HmacSha256),
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
