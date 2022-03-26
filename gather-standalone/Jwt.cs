4c30540c92b19b69c2451f2d2804380554e34ae6
Zefanja Jobse <zefanjaj@gmail.com>
2022-03-26 13:03:45 +0100

    move secret to infile

--- gather-standalone/Jwt.cs
+++ gather-standalone/Jwt.cs
@@ -2,7 +2,6 @@
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using System.Configuration;
 using System.Security.Claims;
 
 namespace gather_standalone.Properties
@@ -22,7 +21,7 @@ namespace gather_standalone.Properties
                     new Claim( "post", dataString)
                 }),
                 Expires = now.AddMinutes(3),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ConfigurationManager.AppSettings["jwtSecret"])), SecurityAlgorithms.HmacSha256),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SUPERSECRETPLACEHOLDER")), SecurityAlgorithms.HmacSha256),
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
